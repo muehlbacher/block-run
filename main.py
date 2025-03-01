d3c289a64664e0ace22c08a7e9b2300dc7b4c8db
Dominik Mühlbacher <muehlbacher.dominik@gmail.com>
2025-03-01 14:34:22 +0100

    change api

--- main.py
+++ main.py
@@ -7,25 +7,28 @@ import json
 load_dotenv()
 
 
-RPC_URL = os.getenv("RPC_URL")
+# RPC_URL = os.getenv("RPC_URL")
+RPC_URL = os.getenv("QUICK_URL")
 DEX_ROUTER_ADDRESS = os.getenv("DEX_ROUTER_ADDRESS")
 
 # Load necessary ABIs
-with open("router_abi.json", "r") as f:
-    router_abi = json.load(f)
+# with open("router_abi.json", "r") as f:
+#    router_abi = json.load(f)
 
 
 w3 = Web3(Web3.HTTPProvider(RPC_URL))
-print(w3.is_connected())
-print(w3.eth.get_block("latest"))
+# print(w3.is_connected())
+# print(w3.eth.get_block("latest"))
 print("---" * 20)
 
 
 if __name__ == "__main__":
-    router_contract = w3.eth.contract(address=DEX_ROUTER_ADDRESS, abi=router_abi)
-    print(router_contract)
+    # router_contract = w3.eth.contract(address=DEX_ROUTER_ADDRESS, abi=router_abi)
+    # print(router_contract)
     pending_tx_filter = w3.eth.filter("pending")
     print(pending_tx_filter)
+    new_transactions = pending_tx_filter.get_new_entries()
+    print(new_transactions)
 
     # payload = {"jsonrpc": "2.0", "method": "eth_blockNumber", "params": [], "id": 1}
 
